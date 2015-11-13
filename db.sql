5f8539dc936697ad17e5ed7e8aac5ae9b686ce08
gtugablue <gtugablue@gmail.com>
2015-11-13 22:47:13 +0000

    ...

--- db.sql
+++ db.sql
@@ -1,28 +1,53 @@
 PRAGMA foreign_keys = ON;
 
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users
+DROP TABLE IF EXISTS User;
+CREATE TABLE User
 (
   id INTEGER PRIMARY KEY, 
   name VARCHAR, 
   email VARCHAR
 );
 
-DROP TABLE IF EXISTS EventTypes;
-CREATE TABLE EventTypes
+DROP TABLE IF EXISTS EventType;
+CREATE TABLE EventType
 (
   id INTEGER PRIMARY KEY,
   name VARCHAR
 );
 
-DROP TABLE IF EXISTS Events;
-CREATE TABLE Events
+DROP TABLE IF EXISTS Event;
+CREATE TABLE Event
 (
   id INTEGER PRIMARY KEY,
-  type INTEGER REFERENCES EventTypes(id),
+  type INTEGER REFERENCES EventType(id),
   name VARCHAR NOT NULL,
   description VARCHAR NOT NULL,
   date DATETIME,
-  owner INTEGER REFERENCES Users(id),
+  owner INTEGER REFERENCES User(id),
   image BLOB
+);
+
+DROP TABLE IF EXISTS EventRegistration;
+CREATE TABLE EventRegistration
+(
+  idEvent INTEGER REFERENCES Event(id),
+  idUser INTEGER REFERENCES User(id),
+  PRIMARY KEY(idEvent, idUser)
+);
+
+DROP TABLE IF EXISTS EventThread;
+CREATE TABLE EventThread
+(
+  id INTEGER PRIMARY KEY,
+  idEvent INTEGER REFERENCES Event(id),
+  name VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS ThreadComment;
+CREATE TABLE ThreadComment
+(
+  id INTEGER PRIMARY KEY,
+  idThread INTEGER REFERENCES Thread(id),
+  author INTEGER REFERENCES User(id),
+  comment TEXT NOT NULL
 );
