361f6a6dedefde8c0b2d0ffd332d05e247ad1d00
gtugablue <gtugablue@gmail.com>
2015-11-16 23:25:24 +0000

    ...

---
+++ templates/view_event.php
@@ -0,0 +1,16 @@
+<?php
+require_once (__DIR__ . "/../config.php");
+require_once (INCLUDES_PATH . "/events.php");
+
+if (! isset ( $_GET ["id"] )) {
+	http_response_code(400);
+	echo "Missing event ID.";
+} else {
+	$idEvent = $_GET["id"];
+	$event = getEvent($idEvent);
+	echo '<h1>' . $event["name"] . '</h1>';
+	echo '<img src="includes/event_image.php?id=' . $idEvent . '" alt="' . $event["name"] . '" width="256" height="256" />';
+	echo '<p>' . $event["description"] . '</p>';
+	echo '<time>' . $event["date"] . '</time>';
+}
+?>
