ff1899b84e5ec18574b53d1dec64cd26f582f78a
gtugablue <gtugablue@gmail.com>
2015-11-18 09:39:15 +0000

    ...

--- database/connection.php
+++ database/connection.php
@@ -1,9 +1,7 @@
 <?php
 require_once (__DIR__ . "/../config.php");
 try {
-    echo "sqlite:" . DATABASE_PATH . "/db.db";
-    echo "sqlite:C:Users/André/git/LTW/database/db.db";
-	$db = new PDO ("sqlite:C:Users/André/git/LTW/database/db.db");
+	$db = new PDO ("sqlite:" . DATABASE_PATH . "/db.db");
 	$db->setAttribute ( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
 	$db->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 } catch ( PDOException $e ) {
