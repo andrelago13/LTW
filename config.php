04dda5b8d7f68de807cb06126f4a9fa6e57de3fb
aslmbc13 <asl_mbc_13@hotmail.com>
2015-11-18 09:56:42 +0000

    Fix?

--- config.php
+++ config.php
@@ -3,9 +3,9 @@ ini_set ( 'display_errors', 1 );
 ini_set ( 'display_startup_errors', 1 );
 error_reporting ( E_ALL );
 
-$dir_file = dirname(utf8_encode(__FILE__));
+$dir_file = dirname(__FILE__);
 
 defined ( "DATABASE_PATH" ) or define ( "DATABASE_PATH", str_replace('\\', '/', realpath ( $dir_file . '/database' )) );
-defined ( "INCLUDES_PATH" ) or define ( "INCLUDES_PATH", str_replace('\\', '/', realpath ( $dir_file . '/includes' )) );
-defined ( "TEMPLATES_PATH" ) or define ( "TEMPLATES_PATH", str_replace('\\', '/', realpath ( $dir_file . '/templates' )) );
+defined ( "INCLUDES_PATH" ) or define ( "INCLUDES_PATH", str_replace('\\', '/', $dir_file . '/includes' ) );
+defined ( "TEMPLATES_PATH" ) or define ( "TEMPLATES_PATH", str_replace('\\', '/', $dir_file . '/templates' ) );
 ?>
--- database/connection.php
+++ database/connection.php
@@ -1,8 +1,6 @@
 <?php
 require_once (__DIR__ . "/../config.php");
 try {
-    echo "sqlite:" . DATABASE_PATH . "/db.db";
-    echo "sqlite:C:Users/André/git/LTW/database/db.db";
 	$db = new PDO ("sqlite:C:Users/André/git/LTW/database/db.db");
 	$db->setAttribute ( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
 	$db->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
