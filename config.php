8b4535eaa006644ca33bf46c51e87340627e6706
aslmbc13 <asl_mbc_13@hotmail.com>
2015-11-18 09:36:23 +0000

    Fix

--- config.php
+++ config.php
@@ -3,7 +3,9 @@ ini_set ( 'display_errors', 1 );
 ini_set ( 'display_startup_errors', 1 );
 error_reporting ( E_ALL );
 
-defined ( "DATABASE_PATH" ) or define ( "DATABASE_PATH", str_replace('\\', '/', realpath ( dirname(__FILE__) . '/database' )) );
-defined ( "INCLUDES_PATH" ) or define ( "INCLUDES_PATH", str_replace('\\', '/', realpath ( dirname(__FILE__) . '/includes' )) );
-defined ( "TEMPLATES_PATH" ) or define ( "TEMPLATES_PATH", str_replace('\\', '/', realpath ( dirname(__FILE__) . '/templates' )) );
+$dir_file = dirname(utf8_encode(__FILE__));
+
+defined ( "DATABASE_PATH" ) or define ( "DATABASE_PATH", str_replace('\\', '/', realpath ( $dir_file . '/database' )) );
+defined ( "INCLUDES_PATH" ) or define ( "INCLUDES_PATH", str_replace('\\', '/', realpath ( $dir_file . '/includes' )) );
+defined ( "TEMPLATES_PATH" ) or define ( "TEMPLATES_PATH", str_replace('\\', '/', realpath ( $dir_file . '/templates' )) );
 ?>
--- database/connection.php
+++ database/connection.php
@@ -1,7 +1,9 @@
 <?php
 require_once (__DIR__ . "/../config.php");
 try {
-	$db = new PDO ("sqlite:" . DATABASE_PATH . "/db.db");
+    echo "sqlite:" . DATABASE_PATH . "/db.db";
+    echo "sqlite:C:Users/André/git/LTW/database/db.db";
+	$db = new PDO ("sqlite:C:Users/André/git/LTW/database/db.db");
 	$db->setAttribute ( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
 	$db->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 } catch ( PDOException $e ) {
