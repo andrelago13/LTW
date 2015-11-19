d0f1bc5720e2d9b0ef0ec18d03ea0b64c717c2a0
aslmbc13 <asl_mbc_13@hotmail.com>
2015-11-19 23:17:19 +0000

    View event styling

--- templates/view_event.php
+++ templates/view_event.php
@@ -7,10 +7,12 @@ if (! isset ( $_GET ["id"] )) {
 	echo "Missing event ID.";
 } else {
 	$idEvent = $_GET["id"];
+	echo '<div class="event" id="event"' . $idEvent . '">';
 	$event = getEvent($idEvent);
-	echo '<h1>' . $event["name"] . '</h1>';
-	echo '<img src="database/event_image.php?id=' . $idEvent . '" alt="' . $event["name"] . '" width="256" height="256" />';
-	echo '<p>' . $event["description"] . '</p>';
-	echo '<time>' . $event["date"] . '</time>';
+	echo '<h1 id="title">' . $event["name"] . '</h1>';
+	echo '<img id="image" src="database/event_image.php?id=' . $idEvent . '" alt="' . $event["name"] . '" width="256" height="256" />';
+	echo '<p id="description">' . $event["description"] . '</p>';
+	echo '<time id="date">' . $event["date"] . '</time>';
+	echo '</div>';
 }
 ?>
