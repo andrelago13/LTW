65eba82643b4e47159d10a095f04c7315136dc57
gtugablue <gtugablue@gmail.com>
2015-11-23 01:09:00 +0000

    Added image upload button, not doing anything yet.

--- templates/create_event.php
+++ templates/create_event.php
@@ -11,7 +11,8 @@ if (! isUserLoggedIn ()) {
 	$eventTypes = getEventTypes ();
 	?>
 
-<form action="create_event.php" method="post" />
+<form action="create_event.php" method="post"
+	enctype="multipart/form-data" />
 <label for="type">Type:</label>
 <select id="type" name="type">
 <?php
@@ -26,24 +27,27 @@ if (! isUserLoggedIn ()) {
 <div class="name">
 	<label for="name">Name:</label> <input type="text" name="name"
 		id="name"
-		value="<?php if (isset($_POST["name"])) echo $_POST["name"];?>">
+		value="<?php if (isset($_POST["name"])) echo $_POST["name"];?>" required>
 </div>
 <div class="description">
 	<label for="description">Description:</label> <input type="description"
 		name="description" id="description"
-		value="<?php if (isset($_POST["description"])) echo $_POST["description"];?>">
+		value="<?php if (isset($_POST["description"])) echo $_POST["description"];?>" requires>
 </div>
 <div class="date">
 	<label for="date">Date:</label> <input type="datetime" name="date"
 		id="date"
-		value="<?php if (isset($_POST["date"])) echo $_POST["date"];?>">
+		value="<?php if (isset($_POST["date"])) echo $_POST["date"];?>" required>
 </div>
 <div class="public">
-	<label for="public">Public?:</label> <input type="checkbox" id="public"
+	<label for="public">Public?</label> <input type="checkbox" id="public"
 		name="public" <?php if (isset($_POST["public"])) echo ' checked';?>>
 </div>
 <input type="hidden" name="owner"
 	value="<?php echo $_SESSION['userid'];?>" />
+<div class="image">
+	<label for="image"></label> <input type="file" name="image" id="image">
+</div>
 <div class="submit_btn">
 	<button type="submit">Submit</button>
 </div>
