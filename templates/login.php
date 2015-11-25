212caa93ad272d3063a8176e297646db5984b692
gtugablue <gtugablue@gmail.com>
2015-11-25 08:46:38 +0000

    Fix.

--- templates/login.php
+++ templates/login.php
@@ -6,7 +6,7 @@ require (INCLUDES_PATH . "/login_action.php");
 
 <form id="login_form" action="login.php" method="post" />
 	<h1 id="title">Login</h1>
-	<input id="username" type="text" value="" placeholder="Username" id="username" />
-	<input id="password" type="password" value="" placeholder="Password" id="password" />
+	<input id="username" type="text" value="" placeholder="Username" name="username" />
+	<input id="password" type="password" value="" placeholder="Password" name="password" />
 	<button id="submit" type="submit">Submit</button>
 </form>
