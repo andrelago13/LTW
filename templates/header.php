b7470806bc1a2c38e28b1e51f67f8fdcbc5b7be1
gtugablue <gtugablue@gmail.com>
2015-11-25 09:40:00 +0000

    main.js

--- templates/header.php
+++ templates/header.php
@@ -9,6 +9,9 @@ require_once (INCLUDES_PATH . "/authentication.php");
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Event Manager</title>
 <link rel="stylesheet" href="styles/style.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
+<script src="scripts/main.js"></script>
 </head>
 <body>
 	<header id="pagetop">
