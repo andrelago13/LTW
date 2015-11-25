b47d69a827e7b48934ae33c95d98d1d9ffa79978
aslmbc13 <asl_mbc_13@hotmail.com>
2015-11-25 23:25:53 +0000

    Styles

--- templates/login.php
+++ templates/login.php
@@ -6,7 +6,15 @@ require (INCLUDES_PATH . "/login_action.php");
 
 <form id="login_form" action="login.php" method="post" />
 	<h1 id="title">Login</h1>
-	<input id="custom_us" name="username" type="text" value="" placeholder="Username" />
-	<input id="custom_pw" name="password" type="password" value="" placeholder="Password" />
+	
+	<div class="form_row" id="login_username_row">
+		<div class="form_row_icon" id="login_username_icon">test</div><!--
+		--><input id="login_username" name="username" type="text" value="" placeholder="Username" />
+	</div>
+	<div class="form_row" id="login_password_row">
+		<div class="form_row_icon" id="login_password_icon">test</div><!--
+		--><input id="login_password" name="password" type="password" value="" placeholder="Password" />
+	</div>
+		
 	<button id="submit" type="submit">Submit</button>
 </form>
--- templates/register.php
+++ templates/register.php
@@ -6,10 +6,26 @@ require_once (INCLUDES_PATH . "/authentication.php");
 
 <form class="register_form" id="register_form" action="login.php" method="post" autocomplete="off" />
 	<h1 id="title">Register</h1>
-	<input id="custom_nm" name="name" type="text" value="" placeholder="Name" autocomplete="off" />
-	<input id="custom_em" name="email" type="text" value="" placeholder="Email" autocomplete="off"/>
-	<input id="custom_us" name="username" type="text" value="" placeholder="Username" autocomplete="off" />
-	<input id="custom_pw" name="password" type="password" value="" placeholder="Password" autocomplete="off" />
-	<input id="custom_cpw" name="confirm_password" type="password" placeholder="Confirm password" autocomplete="off" />
+	
+	<div class="form_row" id="reg_name_row">
+		<div class="form_row_icon" id="reg_name_icon">test</div><!-- Este comentário mantém os elementos em linha
+		--><input class="register_name" id="reg_name" name="name" type="text" value="" placeholder="Name" />
+	</div>
+	<div class="form_row" id="reg_email_row">
+		<div class="form_row_icon" id="reg_email_icon">test</div><!--
+		--><input id="reg_email" name="email" type="text" value="" placeholder="Email" />
+	</div>
+	<div class="form_row" id="reg_username_row">
+		<div class="form_row_icon" id="reg_username_icon">test</div><!--
+		--><input id="reg_username" name="username" type="text" value="" placeholder="Username" />
+	</div>
+	<div class="form_row" id="reg_password_row">
+		<div class="form_row_icon" id="reg_password_icon">test</div><!--
+		--><input id="reg_password" name="password" type="password" value="" placeholder="Password" />
+	</div>
+	<div class="form_row" id="reg_password_conf_row">
+		<div class="form_row_icon" id="reg_password_conf_icon">test</div><!--
+		--><input id="reg_confirm_password" name="confirm_password" type="password" placeholder="Confirm password" />
+	</div>
 	<button id="submit" type="submit">Submit</button>
 </form>
