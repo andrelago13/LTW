c67ff7c3b6d25f74c648af86daa71bd357c180c6
gtugablue <gtugablue@gmail.com>
2015-11-29 00:39:51 +0000

    User profile. TODO: Edit.

--- scripts/user.js
+++ scripts/user.js
@@ -1,41 +1,11 @@
 $(document).ready(function() {
-	eventBriefEllipsis();
-	eventInlineEdit();
+	userInlineEdit();
 });
 
-function eventBriefEllipsis() {
-	$('.event_brief p').each(function() {
-		var numCharsToShow = 100;
-		var content = $(this).html();
-		if(content.length > numCharsToShow) {
-			var lessContent = content.substr(0, numCharsToShow);
-			var moreContent = content.substr(numCharsToShow, content.length - numCharsToShow);
-			var newContent = lessContent;
-			newContent += '<span class="ellipsis">...</span><span class="more_content"><span>';
-			newContent += moreContent;
-			newContent += '</span>&nbsp;<a href="" class="more_link">more</a></span>';
-			$(this).html(newContent);
-		}
-	});
 
-	$(".more_link").click(function(){
-		$(this).parent().prev().toggle(); // .ellipsis span
-		$(this).prev().toggle(); // .moreContent span
-
-		if($(this).hasClass("less")) { // Retract
-			$(this).removeClass("less");
-			$(this).html("more");
-		} else { // Expand
-			$(this).addClass("less");
-			$(this).html("less");
-		}
-		return false;
-	});
-}
-
-function eventInlineEdit() {
+function userInlineEdit() {
 	var name = name;
-	$(".event #name + a.edit").click(function() {
+	$(".user_profile #name + a.edit").click(function() {
 		editTextField($(this).prev(), "name", function(inputElement) {
 			return eventTestName(inputElement.val()).length === 0;
 		});
