f2c19d4c08c641df156d63eeb1c52b29f3e674a4
gtugablue <gtugablue@gmail.com>
2015-11-29 19:16:30 +0000

    Started working on comments.

---
+++ database/comment.php
@@ -0,0 +1,22 @@
+<?php
+require_once (__DIR__ . "/../config.php");
+require_once (DATABASE_PATH . "/connection.php");
+function getComments($idEvent, $amount = -1, $offset = 0) {
+	global $db;
+	$query = "SELECT * FROM Comment INNER JOIN Event ON Comment.idEvent = :event LIMIT :amount OFFSET :offset";
+	$stmt = $db->prepare ( $query );
+	$stmt->bindParam ( ':event', $idEvent, PDO::PARAM_INT );
+	$stmt->bindParam ( ':amount', $amount, PDO::PARAM_INT );
+	$stmt->bindParam ( ':offset', $offset, PDO::PARAM_INT );
+	$stmt->execute ();
+	return $stmt->fetchAll ();
+}
+function getComment($idComment) {
+	global $db;
+	$query = "SELECT * FROM Comment WHERE id = :comment";
+	$stmt = $db->prepare ( $query );
+	$stmt->bindParam ( ':coment', $idComment, PDO::PARAM_INT );
+	$stmt->execute ();
+	return $stmt->fetch ();
+}
+?>
