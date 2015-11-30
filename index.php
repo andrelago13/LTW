d3d7db49ff02e6d5f582144e583bf31819b93f2b
aslmbc13 <asl_mbc_13@hotmail.com>
2015-11-30 16:05:45 +0000

    Changed start page

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-require_once ("login.php");
+require_once (__DIR__ . "/config.php");
+require_once (TEMPLATES_PATH . "/utils.php");
+require_once (INCLUDES_PATH . "/authentication.php");
+
+if(isUserLoggedIn()) {
+	require_once(__DIR__ . "/my_events.php");
+} else {
+	require_once ("login.php");
+}
 ?>
