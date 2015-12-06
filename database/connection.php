b92f74c63402de61b21d823a057d0e3bb556ce76
Gustavo Silva <gtugablue@gmail.com>
2015-12-06 19:19:55 +0000

    Fix DB connection slash.

--- database/connection.php
+++ database/connection.php
@@ -1,7 +1,7 @@
 <?php
 require_once (__DIR__ . "/../config.php");
 try {
-	$p = utf8_encode("sqlite:" . DATABASE_PATH . "\db.db");
+	$p = utf8_encode("sqlite:" . DATABASE_PATH . "/db.db");
 	$db = new PDO ($p);
 	$db->setAttribute ( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
 	$db->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
