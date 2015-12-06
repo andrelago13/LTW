9b14d3b07206aeed20a2405b6f11cd7478874212
Gustavo Silva <gtugablue@gmail.com>
2015-12-06 19:33:59 +0000

    Added support for PHP 5.4.

--- config.php
+++ config.php
@@ -8,6 +8,7 @@ $dir_file = dirname ( __FILE__ );
 defined ( "DATABASE_PATH" ) or define ( "DATABASE_PATH", str_replace ( '\\', '/', realpath ( $dir_file . '/database' ) ) );
 defined ( "INCLUDES_PATH" ) or define ( "INCLUDES_PATH", str_replace ( '\\', '/', $dir_file . '/includes' ) );
 defined ( "TEMPLATES_PATH" ) or define ( "TEMPLATES_PATH", str_replace ( '\\', '/', $dir_file . '/templates' ) );
+defined ( "LIBRARIES_PATH" ) or define ( "LIBRARIES_PATH", INCLUDES_PATH . '/libs' );
 
 defined ( "MAX_IMAGE_SIZE" ) or define ( "MAX_IMAGE_SIZE", 2 * 1024 * 1024 );
 
@@ -15,5 +16,5 @@ session_start ();
 if (! defined ( "NO_SESSION_REGENERATION" ))
 	session_regenerate_id ( true ); // To prevent session fixation
 if (! isset ( $_SESSION ['csrf_token'] ))
-		$_SESSION['csrf_token'] = base64_encode( openssl_random_pseudo_bytes(32));
+	$_SESSION ['csrf_token'] = base64_encode ( openssl_random_pseudo_bytes ( 32 ) );
 ?>
